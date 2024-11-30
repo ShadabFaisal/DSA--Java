128fbf031451bb1cc418448825b54d1aac4ea05e
ShadabFaisal <shadabfaisal91@gmail.com>
2024-11-30 18:17:42 +0530

    Added Graph BFS & DFS Traversal code!

---
+++ DSA/src/Graphs/DFSTraversal.java
@@ -0,0 +1,35 @@
+package Graphs;
+
+import java.util.ArrayList;
+
+public class DFSTraversal {
+
+	public static void main(String[] args) {
+		 ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+	        for (int i = 0; i < 5; i++) {
+	            adj.add(new ArrayList < > ());
+	        }
+	        adj.get(0).add(1);
+	        adj.get(1).add(0);
+	        adj.get(0).add(4);
+	        adj.get(4).add(0);
+	        adj.get(1).add(2);
+	        adj.get(2).add(1);
+	        adj.get(1).add(3);
+	        adj.get(3).add(1);
+	        
+	       
+	        ArrayList < Integer > ans = dfsOfGraph(5, adj);
+	        int n = ans.size(); 
+	        for(int i = 0;i<n;i++) {
+	            System.out.print(ans.get(i)+" "); 
+	        }
+
+	}
+
+	private static ArrayList<Integer> dfsOfGraph(int i, ArrayList<ArrayList<Integer>> adj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
