3f58a48ff18dad106cd5acea24ab66b5b6ef88ad
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-19 23:16:12 +0530

    Added ArraytoLinkedList code, Leetcode 1021 Remove Outermost Parentheses Java code.

---
+++ DSA/src/LinkedList/ArrayToLinkedList.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class ArrayToLinkedList {
+	
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4,5};
+		Node head=constructLL(arr);
+		while(head!=null) {
+			System.out.print(head.data+"->");
+			head=head.next;
+		}
+	}
+
+	
+
+	public static Node constructLL(int arr[]) {
+		// code here
+		Node head = new Node();
+		Node dummy = head;
+		for (int i = 0; i < arr.length; i++) {
+			Node node = new Node(arr[i]);
+			dummy.next = node;
+			dummy = dummy.next;
+		}
+		dummy.next = null;
+		return head.next;
+	}
+
+}
---
+++ DSA/src/LinkedList/Node.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+class Node {
+		int data;
+		Node next;
+
+		Node() {
+			data = 0;
+		}
+
+		Node(int d) {
+			data = d;
+		} // constructor to create a new node
+	}
