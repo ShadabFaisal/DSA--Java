e80a5d31384fd743999e1b8af86b71c6455968c1
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-19 23:16:51 +0530

    Formatted code.

--- DSA/src/LinkedList/ArrayToLinkedList.java
+++ DSA/src/LinkedList/ArrayToLinkedList.java
@@ -1,17 +1,15 @@
 package LinkedList;
 
 public class ArrayToLinkedList {
-	
+
 	public static void main(String[] args) {
-		int[] arr= {1,2,3,4,5};
-		Node head=constructLL(arr);
-		while(head!=null) {
-			System.out.print(head.data+"->");
-			head=head.next;
+		int[] arr = { 1, 2, 3, 4, 5 };
+		Node head = constructLL(arr);
+		while (head != null) {
+			System.out.print(head.data + "->");
+			head = head.next;
 		}
 	}
-
-	
 
 	public static Node constructLL(int arr[]) {
 		// code here
