450e311e6ff6daa1066e5a9ffea8b37d2b054509
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-23 13:49:59 +0530

    Added Java code for Linear Search

--- DSA/src/Searching/LinearSearch.java
+++ DSA/src/Searching/LinearSearch.java
@@ -3,8 +3,20 @@ package Searching;
 public class LinearSearch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arr[] = {1, 2, 3, 4, 6};
+		int k = 6;
+		System.out.println("Array Element found at index: "+linearSearch(arr,k));
 
 	}
+	static int linearSearch(int arr[], int k) {
+        // Your code here
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==k){
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
