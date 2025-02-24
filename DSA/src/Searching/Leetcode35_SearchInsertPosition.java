c3e0647c2777c69436fbd645335cf9bcd241299a
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-24 23:02:14 +0530

    Added Java code to find lower bound, upperbound, Search Insert Position.

---
+++ DSA/src/Searching/Leetcode35_SearchInsertPosition.java
@@ -0,0 +1,33 @@
+package Searching;
+
+public class Leetcode35_SearchInsertPosition {
+
+	public static void main(String[] args) {
+		int[] nums = {1,3,5,6};
+		int target = 2;
+		
+		System.out.println(searchInsert(nums,target));
+
+	}
+	
+	public static int searchInsert(int[] arr, int target) {
+        int ans=arr.length;
+        int low=0;
+        int high=arr.length-1;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=target){
+                ans=mid;//1
+                high=mid-1;//0
+            }
+            else{
+                low=mid+1;
+            }
+
+        }
+
+        return ans;
+    }
+
+}
---
+++ DSA/src/Searching/UpperBound.java
@@ -0,0 +1,31 @@
+package Searching;
+
+public class UpperBound {
+
+	public static void main(String[] args) {
+		int[] arr= {1,2,3,4,5,6,8,10,11};
+		System.out.println(upperBound(arr,arr.length,7));
+
+	}
+	
+	public static int upperBound(int []arr, int x, int n){
+        int ans=n;
+       int low=0;
+       int high=n-1;
+
+       while(low<=high){
+           int mid=low+(high-low)/2;
+           if(arr[mid]>x){
+               ans=mid;//1
+               high=mid-1;//0
+           }
+           else{
+               low=mid+1;
+           }
+
+       }
+
+       return ans;
+   }
+
+}
