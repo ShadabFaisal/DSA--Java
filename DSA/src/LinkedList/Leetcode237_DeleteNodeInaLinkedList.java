9441e78e5b50a3e2402a84937e7a074b8683e243
Shadab Faisal <shadabfaisal91@gmail.com>
2025-02-26 23:12:01 +0530

    Added Java code for Leetcode 237 Delete Node in a linkedlist.

---
+++ DSA/src/LinkedList/Leetcode237_DeleteNodeInaLinkedList.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+public class Leetcode237_DeleteNodeInaLinkedList {
+
+	public static void main(String[] args) {
+		int[] arr = { 4,5,1,9};
+		Node head = ArrayToLinkedList.constructLL(arr);
+		Node toDelete=head.next.next;
+		deleteNode(toDelete);
+		ArrayToLinkedList.printLinkedList(head);
+	}
+	
+	public static void deleteNode(Node node) {
+        if(node==null)
+            return;
+        node.data=node.next.data;
+        node.next=node.next.next;
+        return;
+    }
+
+}
