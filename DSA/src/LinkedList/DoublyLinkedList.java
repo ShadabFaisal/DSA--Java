efbced2e90803ffe32e6cda36cd83e2bfc4319ff
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-13 15:27:17 +0530

    Added Java code to convert an array to doubly linked list and print it.

---
+++ DSA/src/LinkedList/DoublyLinkedList.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+public class DoublyLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] arr= {2,3,4,5,6,7};
+		DLL_Node head=constructDLL(arr);
+		printDLL(head);
+
+	}
+	
+	private static void printDLL(DLL_Node head) {
+		// TODO Auto-generated method stub
+		DLL_Node temp=head;
+		while(temp!=null) {
+			System.out.print(temp.data+"<=>");
+			temp=temp.next;
+		}
+		
+	}
+
+	public static DLL_Node constructDLL(int[] arr) {
+		
+		DLL_Node head=new DLL_Node(arr[0]);
+		DLL_Node prev=head;
+		
+		for(int i=1;i<arr.length;i++) {
+			DLL_Node temp=new DLL_Node(arr[i]);
+			temp.next=null;
+			temp.back=prev;
+			prev.next=temp;
+			prev=temp;
+		}
+	
+		return head;
+	}
+
+}
