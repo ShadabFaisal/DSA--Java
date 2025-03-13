dda17ece5cd54e86e547558c2f5b8ae7846f1ff1
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-13 19:05:58 +0530

    Added Java code to delete a Node in doubly LinkedList

--- DSA/src/LinkedList/DoublyLinkedList.java
+++ DSA/src/LinkedList/DoublyLinkedList.java
@@ -4,111 +4,127 @@ public class DoublyLinkedList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int[] arr= {2,3,4,5,6,7};
-		DLL_Node head=constructDLL(arr);
+
+		int[] arr = { 2, 3, 4, 5, 6, 7 };
+		DLL_Node head = constructDLL(arr);
 		printDLL(head);
-		
-		head=deleteHeadofDLL(head);
+
+		head = deleteHeadofDLL(head);
 		printDLL(head);
-		
-		head=deleteTailofDLL(head);
+
+		head = deleteTailofDLL(head);
 		printDLL(head);
-		
-		head=deleteKthNode(head, 3);
+
+		head = deleteKthNode(head, 3);
+		printDLL(head);
+
+		deleteNode(head.next);
 		printDLL(head);
 
 	}
-	
+
 	private static void printDLL(DLL_Node head) {
 		// TODO Auto-generated method stub
-		DLL_Node temp=head;
-		while(temp!=null) {
-			System.out.print(temp.data+"<->");
-			temp=temp.next;
+		DLL_Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "<->");
+			temp = temp.next;
 		}
 		System.out.println();
-		
+
 	}
 
 	private static DLL_Node constructDLL(int[] arr) {
-		
-		DLL_Node head=new DLL_Node(arr[0]);
-		DLL_Node prev=head;
-		
-		for(int i=1;i<arr.length;i++) {
-			DLL_Node temp=new DLL_Node(arr[i]);
-			temp.next=null;
-			temp.back=prev;
-			prev.next=temp;
-			prev=temp;
+
+		DLL_Node head = new DLL_Node(arr[0]);
+		DLL_Node prev = head;
+
+		for (int i = 1; i < arr.length; i++) {
+			DLL_Node temp = new DLL_Node(arr[i]);
+			temp.next = null;
+			temp.back = prev;
+			prev.next = temp;
+			prev = temp;
 		}
-	
+
 		return head;
 	}
-	
+
 	private static DLL_Node deleteHeadofDLL(DLL_Node head) {
-		if(head==null || head.next==null) {
+		if (head == null || head.next == null) {
 			return null;
 		}
-		DLL_Node prev=head;
-		head=head.next;
-		head.back=null;
-		prev.next=null;
-		
+		DLL_Node prev = head;
+		head = head.next;
+		head.back = null;
+		prev.next = null;
+
 		return head;
 	}
-	
+
 	private static DLL_Node deleteTailofDLL(DLL_Node head) {
-		if(head==null || head.next==null) {
+		if (head == null || head.next == null) {
 			return null;
 		}
-		DLL_Node tail=head;
-		
-		while(tail.next!=null) {
-			tail=tail.next;
+		DLL_Node tail = head;
+
+		while (tail.next != null) {
+			tail = tail.next;
 		}
-		DLL_Node prev=tail.back;
-		prev.next=null;
-		tail.back=null;
-		
+		DLL_Node prev = tail.back;
+		prev.next = null;
+		tail.back = null;
+
 		return head;
 	}
-	
+
 	private static DLL_Node deleteKthNode(DLL_Node head, int k) {
-		
-		
-		DLL_Node temp=head;
-		int cnt=0;
-		
-		while(temp!=null) {
+
+		DLL_Node temp = head;
+		int cnt = 0;
+
+		while (temp != null) {
 			cnt++;
-			if(cnt==k) {
+			if (cnt == k) {
 				break;
 			}
-			temp=temp.next;
+			temp = temp.next;
 		}
-		DLL_Node prev=temp.back;
-		DLL_Node front=temp.next;
-		
-		if(prev==null && front==null) {
+		DLL_Node prev = temp.back;
+		DLL_Node front = temp.next;
+
+		if (prev == null && front == null) {
 			return null;
-		}
-		else if(prev==null) {
+		} else if (prev == null) {
 			return deleteHeadofDLL(head);
-		}
-		else if(front==null) {
+		} else if (front == null) {
 			return deleteTailofDLL(head);
+		} else {
+			prev.next = front;
+			front.back = prev;
+			temp.next = null;
+			temp.back = null;
 		}
-		else {
-			prev.next=front;
-			front.back=prev;
-			temp.next=null;
-			temp.back=null;
-		}
-		
+
 		return head;
-		
+
+	}
+
+	private static void deleteNode(DLL_Node temp) {
+		DLL_Node prev = temp.back;
+		DLL_Node front = temp.next;
+
+		if (front == null) {
+			prev.next = null;
+			temp.back = null;
+			return;
+		}
+
+		prev.next = front;
+		front.back = prev;
+
+		temp.next = temp.back = null;
+
 	}
 
 }
