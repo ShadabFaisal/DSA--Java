c92eca6202559f8d75717b55459babf3e6b88e15
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-14 16:53:48 +0530

    Added Java Code for Leetcode 50 Pow(x,n)

---
+++ DSA/src/Recursion/Leetcode50_PowXN.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+public class Leetcode50_PowXN {
+
+	public static void main(String[] args) {
+		double x=2.0;
+		int n=10;
+		System.out.println(myPow(x,n));
+
+	}
+	
+	public static double myPow(double x, int n) {
+        double ans=1.0;
+        long nn=n;
+        if(nn<0)nn=-1*nn;
+        while(nn>0){
+            if(nn%2==1){
+                ans*=x;
+                nn-=1;
+            }
+            else{
+                x=x*x;
+                nn/=2;
+            }
+        }
+
+        if(n<0)
+            ans=(double)1.0/(double)ans;
+
+        return ans;
+    }
+
+}
