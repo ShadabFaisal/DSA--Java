ee33cb346afb529ccf3b2ca625397a75a6d8bc5b
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-18 04:28:15 +0530

    Added Java code for Leetcode 153. Find Minimum in rotated sorted array.

---
+++ DSA/src/Searching/Leetcode153_FindMinInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+package Searching;
+
+public class Leetcode153_FindMinInRotatedSortedArray {
+
+	public static void main(String[] args) {
+		int[] nums = { 3, 4, 5, 0, 1, 2 };
+		System.out.println(findMin(nums));
+
+	}
+
+	public static int findMin(int[] nums) {
+		int low = 0;
+		int high = nums.length - 1;
+		int ans = Integer.MAX_VALUE;
+
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+
+			// Identify the sorted half
+			// If Left is sorted pick the minimum i.e. nums[low]
+			if (nums[low] <= nums[mid]) {
+				ans = Math.min(ans, nums[low]);
+				low = mid + 1;
+			} else {
+				ans = Math.min(ans, nums[mid]);
+				high = mid - 1;
+			}
+
+		}
+		return ans;
+	}
+
+}
