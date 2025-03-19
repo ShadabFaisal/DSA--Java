e15b312d9b02499bd77062481ab058bb17dcc903
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-20 01:22:37 +0530

    Refactored code!

--- DSA/src/LinkedList/Leetcode876_MiddleOfTheLinkedList.java
+++ DSA/src/LinkedList/Leetcode876_MiddleOfTheLinkedList.java
@@ -3,40 +3,27 @@ package LinkedList;
 public class Leetcode876_MiddleOfTheLinkedList {
 
 	public static void main(String[] args) {
-		ListNode head=new ListNode(1);
-		head=insertAtLast(head,2);
-		head=insertAtLast(head,3);
-		head=insertAtLast(head,4);
-		head=insertAtLast(head,5);
-		head=insertAtLast(head,6);
-		
+		ListNode head = new ListNode(1);
+
+		head = head.insertAtLast(head, 2);
+		head = head.insertAtLast(head, 3);
+		head = head.insertAtLast(head, 4);
+		head = head.insertAtLast(head, 5);
+		head = head.insertAtLast(head, 6);
+
 		System.out.println(middleNode(head).val);
 
 	}
-	
+
 	public static ListNode middleNode(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
+		ListNode slow = head;
+		ListNode fast = head;
 
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        return slow;
-    }
-	
-	public static ListNode insertAtLast(ListNode head, int value) {
-		if (head == null) {
-			return new ListNode(value);
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
 		}
-
-		ListNode temp = head;
-		while (temp.next != null) {
-			temp = temp.next;
-		}
-		ListNode newNode = new ListNode(value);
-		temp.next = newNode;
-		return head;
+		return slow;
 	}
 
 }
