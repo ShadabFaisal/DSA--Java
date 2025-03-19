38fe4ea64fccb0efc2b0a4810bbc5f1b3246b84a
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-20 01:23:45 +0530

    Added insertAtLast function and findTailNode functionto the ListNode.

--- DSA/src/LinkedList/ListNode.java
+++ DSA/src/LinkedList/ListNode.java
@@ -8,5 +8,26 @@ public class ListNode {
 		val = x;
 	}
 	
+	public ListNode insertAtLast(ListNode head, int value) {
+		if (head == null) {
+			return new ListNode(value);
+		}
+
+		ListNode temp = head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		ListNode newNode = new ListNode(value);
+		temp.next = newNode;
+		return head;
+	}
+	public ListNode findTailNode(ListNode head) {
+		ListNode temp=head;
+		while(temp.next!=null) {
+			temp=temp.next;
+		}
+		return temp;
+	}
+	
 	
 }
