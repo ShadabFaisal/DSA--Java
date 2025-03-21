c00fe16102f303ae40b272f54b450c738e411662
Shadab Faisal <shadabfaisal91@gmail.com>
2025-03-22 04:43:22 +0530

    Added Java code for Leetcode 75. Sort Colors (Dutch National Flag Algo)

---
+++ DSA/src/Arrays/Leetcode75_SortColors.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Leetcode75_SortColors {
+
+	// Dutch National Flag Algorithm
+	public static void main(String[] args) {
+		int[] nums = { 2, 0, 2, 1, 1, 0 };
+
+		sortColors(nums);
+
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+	public static void sortColors(int[] nums) {
+		int low = 0;
+		int mid = 0;
+		int high = nums.length - 1;
+
+		while (mid <= high) {
+			if (nums[mid] == 0) {
+				swap(nums, low, mid);
+				low++;
+				mid++;
+			} else if (nums[mid] == 1) {
+				mid++;
+			} else {
+				swap(nums, mid, high);
+				high--;
+			}
+
+		}
+	}
+
+	public static void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+
+}
