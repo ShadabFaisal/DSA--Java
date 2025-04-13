cf61bd282ef43cb27e41640d21aa3d31f7c07e4c
Shadab Faisal <shadabfaisal91@gmail.com>
2025-04-13 11:19:01 +0530

    Added Java code for Leetcode 53. Maximum Subarray (Kadane's Algorithm)

---
+++ DSA/src/Arrays/Leetcode53_MaximumSubarray.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class Leetcode53_MaximumSubarray {
+
+	public static void main(String[] args) {
+		int[] nums= {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(maxSubArray(nums));
+
+	}
+
+	// Kadane's Algorithm
+
+	public static int maxSubArray(int[] nums) {
+		int maxi = Integer.MIN_VALUE;
+
+		int sum = 0;
+
+		for (int num : nums) {
+			sum += num;
+			maxi = Math.max(maxi, sum);
+			if (sum < 0) {
+				sum = 0;
+			}
+		}
+		return maxi;
+
+	}
+
+}
