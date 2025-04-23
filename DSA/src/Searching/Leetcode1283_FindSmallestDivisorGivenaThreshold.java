22aa2581fc389b1c3ab8389cd4f657df36f82ade
Shadab Faisal <shadabfaisal91@gmail.com>
2025-04-23 10:45:53 +0530

    Added Java code for Leetcode 1283. Find Smallest Divisor given threshold

---
+++ DSA/src/Searching/Leetcode1283_FindSmallestDivisorGivenaThreshold.java
@@ -0,0 +1,41 @@
+package Searching;
+
+public class Leetcode1283_FindSmallestDivisorGivenaThreshold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	 public int smallestDivisor(int[] nums, int threshold) {
+	        int maxi=0;
+	        for(int n:nums){
+	            maxi=Math.max(maxi,n);
+	        }
+
+	        int low=1,high=maxi;
+	       int sum=0;
+	       int ans=Integer.MAX_VALUE;
+	       while(low<=high){
+	           int mid=low+(high-low)/2;
+	          sum=findSumOfDivisors(nums,mid);
+	           if(sum<=threshold){
+	               ans=mid;
+	               high=mid-1;
+	           }
+	           else{
+	               low=mid+1;
+	           }
+	       }
+	       return ans;
+	    }
+
+	    public static int findSumOfDivisors(int[] nums, int hourly ){
+	      int sum=0;
+	      for(int i=0;i<nums.length;i++){
+	       sum+=Math.ceil((double) nums[i]/ (double) hourly);
+	      }
+	       return sum;
+	   }
+
+}
