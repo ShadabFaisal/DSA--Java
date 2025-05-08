51ca5869890b2574dad1904234176fc4285f2fdc
Shadab Faisal <shadabfaisal91@gmail.com>
2025-05-08 20:24:51 +0530

    Added Recursive code for Leetcode 78. Subsets

--- DSA/src/Recursion/Leetcode78_Subsets.java
+++ DSA/src/Recursion/Leetcode78_Subsets.java
@@ -28,5 +28,25 @@ public class Leetcode78_Subsets {
 		return ans;
 
 	}
+	 public List<List<Integer>> subsetsUsingRecursion(int[] nums) {
+         List<List<Integer>> ans=new ArrayList<>();
+         helper(0,nums,nums.length,ans,new ArrayList<>());
+         return ans;
+    }
+
+    public void helper(int i, int[] nums, int n, List<List<Integer>> ans, List<Integer> ds){
+       if(i==n) {
+			ans.add(new ArrayList<>(ds));
+			return;
+		}
+		
+		//Pick the element
+        ds.add(nums[i]);
+		helper(i+1,nums,n,ans,ds);
+		ds.remove(ds.size()-1);
+		//Not pick the element
+		helper(i+1,nums,n,ans,ds);
+    }
+    
 
 }
